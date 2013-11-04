4bc3f1094603015598a58b434c0b0a0a263be4a8
standuino <standuino@gmail.com>
2013-11-04 13:46:56 +0100

    new samples

--- renderAudio.ino
+++ renderAudio.ino
@@ -6,7 +6,7 @@ int updateAudio(){
   long output=0;
 
   for(int voice=0;voice<NUMBER_OF_VOICES;voice++){
-   if(aSample[voice].isPlaying()) output+=(volume[voice]*(aSample[voice].next()|crush[voice]))>>8;
+   if(aSample[voice].isPlaying()) output+=(volume[voice]*(aSample[voice].next()^crush[voice]))>>7;
   }
 
 
